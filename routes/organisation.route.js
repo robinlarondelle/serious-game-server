0939db2b69d1b8d1bdbd3a6252714a80de165db6
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-06-08 16:29:31 +0200

    removed redundant files

--- routes/organisation.route.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-const organisationController = require("../controllers/organisation.controller")
-
-routes.get("/", organisationController.getAllOrganisations)
-routes.get("/:orgID", organisationController.getOrganisationByID)
-routes.post("/", organisationController.addOrganisation)
-
-module.exports = routes
