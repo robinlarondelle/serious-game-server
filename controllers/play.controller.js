8c3423b081de295547aede5b5cd9761ae36222d7
Sjoerd Schepers <sjoerdschepers@hotmail.nl>
2020-06-09 13:52:02 +0200

    Random commit voor smits met half afgemaakte teringzooi

--- controllers/play.controller.js
+++ controllers/play.controller.js
@@ -15,16 +15,15 @@ module.exports = {
         
         //Saves a new session of the game (=play) and returns the questions of the first level
         play.save().then((session) => {
-            Game.findById(gamePin, {_id: 0, 'questions.category': 0, 'questions.answers.deltaScore': 0}).select({questions: {$elemMatch: {level: 1}}}).then(result => {
-                result = result.toObject()
-                result['gameID'] = session._id
-                res.status(200).json(result).end()
+            getLevel(gamePin, 1, session._id, (result) => {
+                res.status(200).json(result).end();
             })
         }).catch(error => {
             next(new ApiError("Unkown error", error.message, 400));
         })
     },
 
+    //Method for uploading the answers to a play
     insertAnswers(req, res, next){
         let playID = req.params.playID;
         let answers = req.body.answers;
@@ -33,6 +32,7 @@ module.exports = {
             level: req.body.level,
         })
 
+        //Updates the database and returns the correct data to the client
         createLevelAnswers(levelAnswers, answers, playID, (result) => {
             Play.findOneAndUpdate(
                 { _id: playID }, 
@@ -40,15 +40,18 @@ module.exports = {
             }).then(() => {
                 Play.findById(playID).then((play) => {
                     if (req.body.level == 3) {
-                        res.status(200).json("Game finished").end();
+                        //Aggregates the results and returns them
+                        aggregateData(play, (result) => {
+                            play.scores.push({
+                                
+                            })
+                            res.status(200).json(result).end();
+                        })
                     } else {
-                        Game.findById(play.pin).select({questions: {$elemMatch: {level: req.body.level+1}}}).then(result => {
-                            result = result.toObject()
-                            result['gameID'] = playID
-                            res.status(200).json(result).end()
-                        }).catch(error => {
-                            next(new ApiError("Unkown error", error.message, 400));
-                        }) 
+                        //Gets the next level and returns it
+                        getLevel(play.pin, req.body.level+1, playID, (result) => {
+                            res.status(200).json(result).end();
+                        })
                     }
                 })
             })
@@ -56,6 +59,7 @@ module.exports = {
     }
 }
 
+//Makes the game object containing all answers and metadata
 function createLevelAnswers(levelAnswers, answers, playID, callback) {
     let i = 0;
     for(let answer of answers) {       
@@ -74,6 +78,7 @@ function createLevelAnswers(levelAnswers, answers, playID, callback) {
     }
 }
 
+//Gets the metadata (category and deltaScore) of a single answer
 function getMetaData(question, answer, playID, callback) {
     Play.findById({_id: playID}).then(play => {
         Game.findOne({"pin": play.pin}).select({questions: {$elemMatch: {_id: question}}}).then(result => {
@@ -90,4 +95,45 @@ function getMetaData(question, answer, playID, callback) {
             })
         })
     })
+}
+
+function getLevel(pin, level, playID, callback) {
+    Game.findById(pin, {_id: 0, 'questions.category': 0, 'questions.answers.deltaScore': 0, 'questions.level': 0 })
+    .select({questions: {$elemMatch: {level: level}}})
+    .then(result => {
+        result = result.toObject()
+        result['gameID'] = playID
+        result['level'] = level
+        callback(result)
+    })
+}
+
+function aggregateData(play, callback) {
+    let map = {}
+    let i = 0;
+    for (level of play.results) {
+        aggregateLevel(level, () => {
+            i++;
+            if (i == play.results.length) {
+                callback(map)
+            }
+        })
+    }
+
+    function aggregateLevel(level, callback) {
+        let i = 0;
+        for (answer of level.questions) {
+            let key = answer.category
+            let value = answer.deltaScore
+            if (key in map) {
+                map[key] = map[key] + value
+            } else {
+                map[key] = value
+            }
+            i++;
+            if (i == level.questions.length) {
+                callback()
+            }
+        }
+    }
 }
