a2b0b766bd5a466bcc8ae5d032ec8f4bacba7d79
Sjoerd Schepers <sjoerdschepers@hotmail.nl>
2020-06-09 12:01:17 +0200

    Got the play controller to a minimum playable state

--- controllers/play.controller.js
+++ controllers/play.controller.js
@@ -9,15 +9,16 @@ module.exports = {
         let gamePin = req.params.playID;
         const play = new Play({
             _id: new mongoose.Types.ObjectId(),
-            game: gamePin
+            pin: gamePin
         })
         
         //Saves a new session of the game (=play) and returns the questions of the first level
-        play.save().then(() => {
-            console.log("saved results")
-            Game.find({ level: 1 }).then(result => {
-                res.status(200).json(result).end();
-            }).catch(error => res.status(500).json(error).end())
+        play.save().then((session) => {
+            Game.findById(gamePin).select({questions: {$elemMatch: {level: 1}}}).then(result => {
+                result = result.toObject()
+                result['gameID'] = session._id
+                res.status(200).json(result).end()
+            })
         }).catch(error => {
             next(new ApiError("Unkown error", error.message, 400));
         })
@@ -25,82 +26,67 @@ module.exports = {
 
     insertAnswers(req, res, next){
         let playID = req.params.playID;
-        let level = req.body.level;
         let answers = req.body.answers;
 
-        const levelAnswers = new LevelAnswers({
-            level: level,
+        let levelAnswers = new LevelAnswers({
+            level: req.body.level,
         })
 
-        for(let answer of answers) {
-            //Get related category and deltascore
-            let categoryId;
-            let deltaScore;
-            Play.findById(playID).then(result => {
-                Game.findOne({ pin: result.pin }).then(result => {
-                    result.questions.findOne({_id : answer.question}).then(question => {
-                        categoryId = question.category
-                        question.answers.findById(answer.answer).then(ans => {
-                            deltaScore = ans.deltascore
-                        }).catch(error => res.status(500).json(error).end())
-                    }).catch(error => res.status(500).json(error).end())
-                }).catch(error => res.status(500).json(error).end())
-            }).catch(error => res.status(500).json(error).end())
+        createLevelAnswers(levelAnswers, answers, playID, (result) => {
+            Play.findOneAndUpdate(
+                { _id: playID }, 
+                { $push: { results: result }
+            }).then(() => {
+                Play.findById(playID).then((play) => {
+                    if (req.body.level == 3) {
+                        res.status(200).json("Game finished").end();
+                    } else {
+                        Game.findById(play.pin).select({questions: {$elemMatch: {level: req.body.level+1}}}).then(result => {
+                            result = result.toObject()
+                            result['gameID'] = playID
+                            res.status(200).json(result).end()
+                        }).catch(error => {
+                            next(new ApiError("Unkown error", error.message, 400));
+                        }) 
+                    }
+                })
+            })
+        })
+    }
+}
 
-            //Push the answers to the new LevelAnswers object
+function createLevelAnswers(levelAnswers, answers, playID, callback) {
+    let i = 0;
+    for(let answer of answers) {       
+        getMetaData(answer.question, answer.answer, playID, (result) => {
             levelAnswers.questions.push({
                 question: answer.question,
                 answer: answer.answer,
-                category: categoryId,
-                deltaScore: deltaScore
+                category: result.category,
+                deltaScore: result.deltaScore
             })
-        }
-
-        //Push the LevelAnswers object into the array and commit it
-        Play.findOneAndUpdate(
-            { _id: playID }, 
-            { $push: { results: levelAnswers }
-        }).then(() => {
-            if (level == 3) {
-                let dictionary = {}
-                //Aggregate all deltaScores
-                Play.findById({ _id : playID }).then(result => {
-                    for (levelAnswers of result.results) {
-                        for (answer of levelAnswers.questions) {
-                            let key = answer.category
-                            let value = answer.deltaScore
-                            if (key in dictionary) {
-                                dictionary[key] = dictionary[key] + value
-                            } else {
-                                dictionary[key] = value
-                            }
-                        }
-                    }
-                })
-
-                //Put all scores in the play session
-                for (category of dictionary) {
-                    Play.findOneAndUpdate({ _id: playId}, {
-                        $push: { score: {
-                                category: category,
-                                score: dictionary[category]
-                            } 
-                        }
-                    })
-                }
-
-                //Set the finished state to true and return it for analysis
-                Play.findOneAndUpdate({ _id: playId}, 
-                    { finished : true }).then(result => {
-                        res.status(200).json(result).end();
-                })
-            } else {
-                Game.find({ level: level+1 }).then(result => {
-                    res.status(200).json(result).end();
-                }).catch(error => res.status(500).json(error).end())
+            i++;
+            if (i == answers.length) {
+                callback(levelAnswers)
             }
-        }).catch(error => {
-            next(new ApiError("Unkown error", error.message, 500));
-        })
+        })       
     }
+}
+
+function getMetaData(question, answer, playID, callback) {
+    Play.findById({_id: playID}).then(play => {
+        Game.findOne({"pin": play.pin}).select({questions: {$elemMatch: {_id: question}}}).then(result => {
+            category = result.questions[0].category
+        }).then(() => {
+            Game.findOne({"pin": play.pin}).select({questions: {$elemMatch: {_id: question}}}).then(result => {
+                for (let a of result.questions[0].answers) {
+                    if (a._id == answer) {
+                        deltaScore = a.deltaScore
+                        callback({ category: category, deltaScore: deltaScore})
+                        break;
+                    }
+                }
+            })
+        })
+    })
 }
