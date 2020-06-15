71318cf1d8c06fa3217839f5776b6fd37228445f
Bas van Rooten <basvanrooten@me.com>
2020-06-15 22:54:25 +0200

    WIP: Add playsPerDay method

--- routes/stats.route.js
+++ routes/stats.route.js
@@ -3,5 +3,6 @@ const statController = require("../controllers/stats.controller")
 
 routes.get("/avgScorePerCat", statController.avgScorePerCat)
 routes.get("/avgPlayer", statController.avgPlayer)
+routes.get("/:gameID/playsPerDay", statController.playsPerDay)
 
 module.exports = routes
